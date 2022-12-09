9fbc8412ea34207fdf251795ad0d6e66f0076bdf
Kendall Perry <perry.kendall@gmail.com>
2022-12-08 19:01:17 -0500

    fixed issue with the interest, saved variable as an int type

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -8,23 +8,24 @@ public class LoanCalculator {
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
      *
-     * Each iteration takes 10% off the amount, (or 90% of the amount),
-     * Once the while loop has gone through 3 times, the final amount is returned.
+     * Each iteration takes 10% off the amount, saves this in a variable called interest
+     * The counter keeps track of how many times the loop occurs
+     * Once the while loop has gone through, the final amount is returned.
+     * However, if the starting amount is 0 to begin with, 0 is immediately returned
      *
      * @param ???
      * @return ???
      */
     static int getRemainingAmountIn3Months(int amount) {
-        int counter = 0;
-        int finalAmount = amount;
-
         if (amount == 0) return 0;
+        int counter = 0;
+        int remainingAmount = amount;
         while (counter <= 2) {
-            finalAmount *= .9;
+            int interest = (int) (remainingAmount * .10);
+            remainingAmount -= interest;
             counter++;
         }
-
-        return finalAmount;
+        return remainingAmount;
     }
 
     public static void main(String[] args) {
